1e4b28303d8e645e8ba4efe5fc1e04942c6ce4df
Tomo Masakura <tomo.masakura@gmail.com>
2015-08-19 22:06:51 +0900

    Hello, world! milkcoco

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+(function () {
+  'use strict';
+
+  var milkcocoa = new MilkCocoa("postidiray64.mlkcca.com");
+  var dataStore = milkcocoa.dataStore('hello');
+
+  $(document).on('click', '#push', function () {
+    dataStore.push({message: 'Hello!'});
+  });
+
+  dataStore.on('push', function (e) {
+    console.log(e.value.message);
+  });
+})();
